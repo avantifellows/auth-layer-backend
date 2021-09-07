3a95b9256f43ec53a1453d3897683c55723e83e8
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2021-09-07 12:36:47 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ exports.checkForUser = functions.https.onRequest(async (request, response) => {
   //firebase query using where needs a string to compare
   const id = JSON.stringify(param["userID"])
   console.log(db)
-  await db.collection('HaryanaStudents').where('Student Registration Number', '==', id).get().then((query)  => {
+  await db.collection('HaryanaStudents').where('srn_number', '==', id).get().then((query)  => {
     if (query.empty){
       response.send(false)
     }
