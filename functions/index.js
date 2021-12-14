e5cd3e52d450884a66683ea78be4e42cb1c56b5d
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2021-12-14 10:47:18 +0530

    added program support

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,20 @@
 const functions = require("firebase-functions");
 const db = require("./dbClient")
 
+/** This function checks for user in the database */
 exports.checkForUser = functions.https.onRequest(async (request, response) => {
   response.header('Access-Control-Allow-Headers', 'Content-Type');
   response.header('Access-Control-Allow-Origin', '*');
 
-  //parsing the query parameters from the request -> in this case, the userID
+  /** Parsing the query parameters from the request -> in this case, the userID, documentName, columnName */
   let param = JSON.parse(Object.keys(request.body)[0])
 
-  //firebase query using where needs a string to compare
   const id = JSON.stringify(param["userID"])
-  await db.collection('HaryanaStudents').where('srn_number', '==', id).get().then((query)  => {
+  const document = JSON.stringify(param["documentName"])
+  const column = JSON.stringify(param["columnName"])
+
+  /** Query all documents matching the id from the requested collection  */
+  await db.collection(JSON.parse(document)).where(JSON.parse(column), '==', id).get().then((query)  => {
     if (query.empty){
       response.send(false)
     }
@@ -19,4 +23,22 @@ exports.checkForUser = functions.https.onRequest(async (request, response) => {
     }
   });
 });
+
+/** This function retrieves all program details based on the requested program */
+exports.getProgramData = functions.https.onRequest(async (request, response) => {
+  response.header('Access-Control-Allow-Headers', 'Content-Type');
+  response.header('Access-Control-Allow-Origin', '*');
+
+  /** Parsing the query parameters from the request -> in this case, the program name */
+  let param = JSON.parse(Object.keys(request.body)[0])
+
+  /** Query the details against a specific program name from the 'Programs' collection */
+  const id = JSON.stringify(param["program"])
+  await db.collection('Programs').doc(JSON.parse(id)).get().then((query)  => {
+    if(query.exists){
+      response.send(query.data())
+    }
+  });
+});
+
 
