b6d1061870bfe7e0d9d5487277c81f35d90fb810
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-02-21 22:17:33 +0530

    feedbacks and single Http handler to wrap sentry and firebase http

--- functions/index.js
+++ functions/index.js
@@ -7,87 +7,79 @@ Sentry.GCPFunction.init({
   tracesSampleRate: 1.0,
 });
 
-/** This function checks for user in the database */
-exports.checkForUser = functions.https.onRequest(
+const httpRequestHandler = (callback) => functions.https.onRequest(
+  // wrap the callback function with Sentry's http handler
+  // this allows Sentry to capture any errors that occur and report to Sentry dashboard
   Sentry.GCPFunction.wrapHttpFunction(async (request, response) => {
     try {
-      response.header('Access-Control-Allow-Headers', 'Content-Type');
-      response.header('Access-Control-Allow-Origin', '*');
-
-      // Parsing the query parameters from the request
-      let params = JSON.parse(Object.keys(request.body)[0])
-      console.log(params)
-
-      const id = params["userID"]
-      const collection = params["collectionName"]
-      const column = params["columnName"]
-
-      // Query all documents matching the id from the requested collection
-      result = await db.collection(collection).where(column, '==', id.toString()).get()
-      if (result.empty){
-        response.send(false)
-      }
-      else{
-        response.send(true)
-      }
+      return await callback(request, response);
     } catch (error) {
       Sentry.captureException(error);
       response.status(500).send(`Internal error: ${error.message}`);
     }
   })
 );
+
+/** This function checks for user in the database */
+exports.checkForUser = httpRequestHandler(async (request, response) => {
+  response.header('Access-Control-Allow-Headers', 'Content-Type');
+  response.header('Access-Control-Allow-Origin', '*');
+
+  // Parsing the query parameters from the request
+  let params = JSON.parse(Object.keys(request.body)[0])
+  console.log(params)
+
+  const id = params["userID"]
+  const collection = params["collectionName"]
+  const column = params["columnName"]
+
+  // Query all documents matching the id from the requested collection
+  result = await db.collection(collection).where(column, '==', id.toString()).get()
+  if (result.empty){
+    response.send(false)
+  }
+  else{
+    response.send(true)
+  }
+});
 
 /** This function retrieves all group details based on the requested program */
-exports.getGroupData = functions.https.onRequest(
-  Sentry.GCPFunction.wrapHttpFunction(async (request, response) => {
-    try {
-      response.header('Access-Control-Allow-Headers', 'Content-Type');
-      response.header('Access-Control-Allow-Origin', '*');
+exports.getGroupData = httpRequestHandler(async (request, response) => {
+  response.header('Access-Control-Allow-Headers', 'Content-Type');
+  response.header('Access-Control-Allow-Origin', '*');
 
-      //Parsing the query parameters from the request
-      let params = JSON.parse(Object.keys(request.body)[0])
-      console.log(params)
+  //Parsing the query parameters from the request
+  let params = JSON.parse(Object.keys(request.body)[0])
+  console.log(params)
 
-      // Query the details against a specific group name from the 'Groups' collection
-      const id = params["group"]
+  // Query the details against a specific group name from the 'Groups' collection
+  const id = params["group"]
 
-      result = await db.collection('Groups').doc(id.toString()).get()
-      if(result.exists){
-        response.send(result.data())
-      }
-    } catch (error) {
-      Sentry.captureException(error);
-      response.status(500).send(`Internal error: ${error.message}`);
-    }
-  })
-);
+  result = await db.collection('Groups').doc(id.toString()).get()
+  if(result.exists){
+    response.send(result.data())
+  }
+});
 
 /** This function checks for user in the database */
-exports.stagingCheckForUser = functions.https.onRequest(
-  Sentry.GCPFunction.wrapHttpFunction(async (request, response) => {
-    try {
-      response.header('Access-Control-Allow-Headers', 'Content-Type');
-      response.header('Access-Control-Allow-Origin', '*');
+exports.stagingCheckForUser = httpRequestHandler(async (request, response) => {
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
+    response.header('Access-Control-Allow-Origin', '*');
 
-      // Parsing the query parameters from the request
-      let params = JSON.parse(Object.keys(request.body)[0])
-      console.log(params)
+    // Parsing the query parameters from the request
+    let params = JSON.parse(Object.keys(request.body)[0])
+    console.log(params)
 
-      const id = params["userID"]
-      const collection = params["collectionName"]
-      const column = params["columnName"]
+    const id = params["userID"]
+    const collection = params["collectionName"]
+    const column = params["columnName"]
 
-      // Query all documents matching the id from the requested collection
-      result = await db.collection(collection).where(column, '==', id.toString()).get()
-      if (result.empty){
-        response.send(false)
-      }
-      else{
-        response.send(true)
-      }
-    } catch (error) {
-      Sentry.captureException(error);
-      response.status(500).send(`Internal error: ${error.message}`);
+    // Query all documents matching the id from the requested collection
+    result = await db.collection(collection).where(column, '==', id.toString()).get()
+    if (result.empty){
+      response.send(false)
     }
-  })
-);
+    else{
+      response.send(true)
+    }
+});
