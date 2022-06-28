c403420d62ef25f5a9161f87014a0ee939aa171d
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-06-28 13:02:51 +0530

    linting

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
-const user = require('./user');
-const group = require('./group');
-const session = require('./session');
+const user = require("./user");
+const group = require("./group");
+const session = require("./session");
 exports.checkForUser = user.checkForUser;
 exports.getGroupData = group.getGroupData;
 exports.getSessionData = session.getSessionData;
-
-
-
